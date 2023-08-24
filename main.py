8a129fc2c368b0f12668d72c7fa18f0c83864c90
MakMemEng <yatani.mems4568@gmail.com>
2023-08-24 15:44:27 +0900

    ✨ Userモデルの作成

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import logging
 
 import settings
 
-
 logging.basicConfig(filename=settings.LOG_FILE, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
     logger.info("running server")
+
+    from app.models.user import User, database
+
+    session = database.connect_db()
+    u = User()
+    u.name = "test"
+    session.add(u)
+    session.commit()
+    session.close()
