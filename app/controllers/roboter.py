13cc6f87b44fa04f46cedfb08f67af422324f28f
MakMemEng <yatani.mems4568@gmail.com>
2023-08-25 07:22:52 +0900

    💄 プラグインによるValidation, Rendering設定

--- app/controllers/roboter.py
+++ app/controllers/roboter.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+from app.controllers.forms import RateForm
 import settings
 
 app = Flask(__name__, template_folder="../../templates/", static_folder="../../static/")
@@ -19,12 +20,16 @@ ROBOT_NAME = "Roboko"
 def hello() -> str:
     if request.method == "POST":
         user_name = request.form.get("user_name").strip()
-        return render_template("evaluate_restaurant.html", user_name=user_name)
+        form = RateForm(request.form)
+        form.user_name.data = user_name
+        return render_template(
+            "evaluate_restaurant.html", user_name=user_name, form=form
+        )
     return render_template("hello.html", name=ROBOT_NAME)
 
 
 @app.route("/restaurant/rate", methods=["GET", "POST"])
-def restaurant_rate() -> str:
+def restaurant_rate():
     if request.method == "POST":
         user_name = request.form.get("user_name").strip()
         rate = request.form.get("rate").strip()
