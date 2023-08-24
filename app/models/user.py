d52e0fd68f013ee9596abf1bbc29ff81e0bdef0b
MakMemEng <yatani.mems4568@gmail.com>
2023-08-25 07:44:21 +0900

    ✨ User modelにアクセスしてデータをDBへ保存

--- app/models/user.py
+++ app/models/user.py
@@ -4,10 +4,27 @@ from sqlalchemy import (
     UniqueConstraint,
 )
 
-from app.models.db import BaseDatabase
+from app.models.db import BaseDatabase, database
 
 
 class User(BaseDatabase):
     __tablename__ = "user"
     name = Column(String)
     UniqueConstraint(name)
+
+    @staticmethod
+    def get_or_create(name):
+        session = database.connect_db()
+        row = session.query(User).filter(User.name == name).first()
+        if row:
+            session.close()
+            return row
+
+        user = User(name=name)
+        session.add(user)
+        session.commit()
+        # return user  とすると,user.idを取得しないまま,呼び出してしまう
+        # dbにcommitしたデータを再度呼び出す必要がある
+        row = session.query(User).filter(User.name == name).first()
+        session.close()
+        return row
